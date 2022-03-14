7ac2fbc208d0033e3f85a2a04c06e3f2512f0b79
wizovo <manolo0782@gmail.com>
2022-03-14 20:22:13 +0100

    Frontend
    Logo, Inicio, Boton citas.
    Estructura Iconos redes sociales

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:app_reservas/main.dart';
 import 'package:app_reservas/my_app.dart';
 import 'package:flutter/material.dart';
 
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -10,22 +11,49 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('assets/image/JorgeLuqueBarber'),
+              image: AssetImage('assets/images/JLuqueBarber.jpg'),
               fit: BoxFit.cover)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
-          children: [
+          children:[
             Container(
-              padding: const EdgeInsets.all(16),
+              color: Colors.black87,
+              child: ButtonBar(
+              mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  IconButton(
+                    icon: const FaIcon(FontAwesomeIcons.facebookSquare),
+                    color: Colors.blue.shade900,
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const FaIcon(FontAwesomeIcons.instagram),
+                    color: Colors.pink.shade900,
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const FaIcon(FontAwesomeIcons.whatsapp),
+                    color: Colors.green.shade400,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(15),
               width: MediaQuery.of(context).size.width,
               child: ElevatedButton.icon(
-                icon: Icon(Icons.phone),
-                label: Text('LOGIN WITH PHONE'),
+                icon: Icon(
+                  Icons.calendar_today_outlined,
+                  color: Colors.white,),
+                label: Text('COGE CITA AQUI !',style: TextStyle(color: Colors.white)),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black.withOpacity(0.7))
+                ),
                 onPressed: () {},
               ),
             )
